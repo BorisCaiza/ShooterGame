363f514fe19a136cb2651dcfd6be926b81ceed9c
Boris Caiza <77301967+BorisCaiza@users.noreply.github.com>
2021-10-02 13:19:07 -0500

    hasta barras de vida

--- Assets/_Scripts/Life.cs
+++ Assets/_Scripts/Life.cs
@@ -9,6 +9,9 @@ public class Life : MonoBehaviour
     [SerializeField]
     private float amount;
 
+    public float maximumLife = 100f;
+    
+
     public UnityEvent onDeath;
 
     public float Amount
@@ -28,5 +31,8 @@ public class Life : MonoBehaviour
         }
     }
 
- 
+    private void Awake()
+    {
+        amount = maximumLife;
+    }
 }
